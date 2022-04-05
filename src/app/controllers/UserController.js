1947a73cc3b96797fa9e9cebac1efc134e2aea30
lthdt-18clc4-18127201 <50988020+lthdt-18clc4-18127201@users.noreply.github.com>
2022-04-05 22:31:39 +0700

    add admin login, admin ban user, create admin user

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,6 +1,4 @@
-const User = require('../models/User')
 const express = require("express");
-const router = express.Router();
 const User = require("../models/User");
 const bcrypt = require("bcryptjs");
 
--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -2,13 +2,9 @@ const express = require("express");
 const router = express.Router();
 const UserController = require("../app/controllers/UserController");
 
-<<<<<<< HEAD
 router.delete('/:id', UserController.delete);
 router.get('/', UserController.index);
-=======
-router.get("/", UserController.index);
 router.get("/profile", UserController.getProfile);
 router.post("/profile", UserController.updateProfile);
->>>>>>> e142cdb0fc6bd37822faccc57f6673ea44bc604f
 
 module.exports = router;
